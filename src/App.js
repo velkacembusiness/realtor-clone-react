4dcb441c715a079ac8982943994f6c88f17c8fda
Belkacem LAOUI <B.LAOUI@agro-film.loc>
2022-09-16 23:14:29 +0100

    Create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgetPassword from './pages/ForgetPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
    <>
-  <h2 className='text-2xl'>Hello World</h2>
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/forget-password' element={<ForgetPassword />} />
+      <Route path='/offers' element={<Offers />} />
+    </Routes>
+  </Router>
    </>
   );
 }
