d9abec3f0e210b711115da3c92ea38f8f59b69ea
Belkacem LAOUI <velkacembusiness@gmail.com>
2022-09-20 11:48:41 +0100

    complete the sign in functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
-
 export default function OAuth() {
   const navigate = useNavigate();
   async function onGoogleClick() {
@@ -34,9 +32,13 @@ export default function OAuth() {
     }
   }
   return (
-    <button onClick={onGoogleClick} className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bgg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'>
-        <FcGoogle className='text-2xl bg-white rounded-full mr-2' />
-        Continue with Google
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
+      Continue with Google
     </button>
-  )
+  );
 }
